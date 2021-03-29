38c9cbc4a311a01fd252d50c3d5531f0f488a2b3
Stefan Skočo <stefansgit@outlook.com>
2021-03-29 09:49:19 +0200

    fix

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -9,7 +9,7 @@ class Microsoft {
   set name(newName) {
     this._name = newName;
   }
-  ocupation() {
+  occupation() {
     return `${this.name} is a philantropist!`;
   }
 }
@@ -24,12 +24,12 @@ class Facebook extends Microsoft {
   }
 }
 let henry = new Microsoft("William Henry Gates III");
-henry.ocupation();
-console.log(henry.ocupation());
+henry.occupation();
+console.log(henry.occupation());
 let elliot = new Facebook("Mark Elliot Zuckerberg", 23);
-elliot.ocupation();
+elliot.occupation();
 elliot.studentAge();
-console.log(elliot.ocupation());
+console.log(elliot.occupation());
 console.log(elliot.studentAge());
 // Only change code above this line
 module.exports = {
